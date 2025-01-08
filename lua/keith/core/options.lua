5669ff86e4d16a811ed4d1e378f173909daa62d9
Keith Jardiniano <keithanselmo.jardiniano@gmail.com>
2025-01-08 20:53:09 +0800

    correct colors inside tmux and alacritty

--- lua/keith/core/options.lua
+++ lua/keith/core/options.lua
@@ -2,6 +2,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Correct colors
+vim.o.termguicolors = true
+
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 
